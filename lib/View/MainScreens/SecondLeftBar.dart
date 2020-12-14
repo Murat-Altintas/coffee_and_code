356ede72ab4a518e942d1f71f8da45a67c7e54ff
Murat-Altintas <murat.altintas.34@gmail.com>
2020-12-15 01:11:10 +0300

    ver 2.0

--- lib/View/MainScreens/SecondLeftBar.dart
+++ lib/View/MainScreens/SecondLeftBar.dart
@@ -38,148 +38,167 @@ class _SecondLeftBarState extends State<SecondLeftBar> {
       body: SafeArea(
         child: Column(
           children: [
+            //     if(_checkTab ==0)
+            //   _bulildCoffeeWidget( mainTheme),
+            // if(_checkTab ==1)
+            // Text( 'machıne'),
             Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: SizedBox.expand(
-                      child: LayoutBuilder(
-                        builder: (context, constrants) {
-                          double height = constrants.maxHeight;
-                          _pointsPositions = [height / 5, height / 2.05, height / 1.23, height / 1];
-                          _top ??= _pointsPositions[0];
-                          return Stack(
-                            children: [
-                              Positioned(
-                                top: height / 100,
-                                child: _menuTextWidget(
-                                  animationController: (i) {
-                                    _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
-                                  },
-                                  indexChecked: _indexChecked,
-                                  chooseLeftTab: _leftTabs,
-                                  i: 0,
-                                ),
-                              ),
-                              Positioned(
-                                top: height / 3.3,
-                                child: _menuTextWidget(
-                                  animationController: (i) {
-                                    _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
-                                  },
-                                  indexChecked: _indexChecked,
-                                  chooseLeftTab: _leftTabs,
-                                  i: 1,
-                                ),
-                              ),
-                              Positioned(
-                                top: height / 1.7,
-                                child: _menuTextWidget(
-                                  animationController: (i) {
-                                    _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
-                                  },
-                                  indexChecked: _indexChecked,
-                                  chooseLeftTab: _leftTabs,
-                                  i: 2,
-                                ),
-                              ),
-                              Positioned(
-                                top: height / 1.2,
-                                child: _menuTextWidget(
-                                  animationController: (i) {
-                                    _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
-                                  },
-                                  indexChecked: _indexChecked,
-                                  chooseLeftTab: _leftTabs,
-                                  i: 3,
-                                ),
-                              ),
-                              AnimatedPositioned(
-                                duration: Duration(seconds: 1),
-                                curve: Curves.elasticInOut,
-                                top: _top - height / 16,
-                                child: Padding(
-                                  padding: EdgeInsets.only(left: context.width2 * 10),
-                                  child: Container(
-                                    width: context.width2 * 1.5,
-                                    height: context.width2 * 1.5,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: mainTheme.primaryColorLight,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
+                flex: 9,
+                child: PageView(
+                  children: [
+                    _bulildCoffeeWidget(mainTheme),
+                    Center(child: Text('Machines', style: mainTheme.textTheme.headline3),
+                    ),],
+                  controller: _myTabController,
+                )),
+            // list[_checkTab],
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 75),
+                child: SizedBox(
+                  height: context.height2 * 6,
+                  child: BottomBarWidget(
+                    currentTab: _tabChecked,
                   ),
-                  Expanded(
-                    flex: 5,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15),
-                      child: Column(
-                        children: [
-                          SizedBox(height: context.height2 * 6),
-                          Row(
-                            children: [
-                              _leftTabs == 0
-                                  ? Text("Single Origin", style: mainTheme.textTheme.headline4)
-                                  : _leftTabs == 1
-                                      ? Text("Single Serve", style: mainTheme.textTheme.headline4)
-                                      : _leftTabs == 2
-                                          ? Text("Blend", style: mainTheme.textTheme.headline4)
-                                          : _leftTabs == 3
-                                              ? Text("Options Page", style: mainTheme.textTheme.headline4)
-                                              : null,
-                              Spacer(),
-                              IconButton(
-                                splashRadius: 20,
-                                onPressed: () {},
-                                iconSize: context.iconSmall,
-                                icon: Icon(Icons.shopping_basket),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: context.height2 * 6),
-                          Expanded(
-                            child: PageView(
-                              scrollDirection: Axis.vertical,
-                              physics: NeverScrollableScrollPhysics(),
-                              controller: _myPageController,
-                              children: _leftTabs == 3
-                              ///i "just" can see "TEXT Page" text in Enable Widget Select Mode? :/
-                                  ? [Text("TEST Page", style: mainTheme.textTheme.headline4)]
-                                  : coffeesType
-                                      .map((everyItems) => PageView(
-                                            scrollDirection: Axis.horizontal,
-                                            children: everyItems.map<Widget>((e) => ProductWidget(e)).toList(),
-                                          ))
-                                      .toList(),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 75),
-              child: SizedBox(
-                height: context.height2 * 6,
-                child: BottomBarWidget(
-                  currentTab: _tabChecked,
                 ),
               ),
             ),
-            SizedBox(height: context.height2 * 3),
+            //SizedBox(height: context.height2 * 3),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _bulildCoffeeWidget(ThemeData mainTheme) {
+    return Row(
+      children: [
+        Expanded(
+          child: SizedBox.expand(
+            child: LayoutBuilder(
+              builder: (context, constrants) {
+                double height = constrants.maxHeight;
+                _pointsPositions = [height / 5, height / 2.05, height / 1.23, height / 1];
+                _top ??= _pointsPositions[0];
+                return Stack(
+                  children: [
+                    Positioned(
+                      top: height / 100,
+                      child: _menuTextWidget(
+                        animationController: (i) {
+                          _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
+                        },
+                        indexChecked: _indexChecked,
+                        chooseLeftTab: _leftTabs,
+                        i: 0,
+                      ),
+                    ),
+                    Positioned(
+                      top: height / 3.3,
+                      child: _menuTextWidget(
+                        animationController: (i) {
+                          _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
+                        },
+                        indexChecked: _indexChecked,
+                        chooseLeftTab: _leftTabs,
+                        i: 1,
+                      ),
+                    ),
+                    Positioned(
+                      top: height / 1.7,
+                      child: _menuTextWidget(
+                        animationController: (i) {
+                          _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
+                        },
+                        indexChecked: _indexChecked,
+                        chooseLeftTab: _leftTabs,
+                        i: 2,
+                      ),
+                    ),
+                    Positioned(
+                      top: height / 1.2,
+                      child: _menuTextWidget(
+                        animationController: (i) {
+                          _myPageController.animateToPage(i, duration: Duration(seconds: 2), curve: Curves.fastLinearToSlowEaseIn);
+                        },
+                        indexChecked: _indexChecked,
+                        chooseLeftTab: _leftTabs,
+                        i: 3,
+                      ),
+                    ),
+                    AnimatedPositioned(
+                      duration: Duration(seconds: 1),
+                      curve: Curves.elasticInOut,
+                      top: _top - height / 16,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: context.width2 * 10),
+                        child: Container(
+                          width: context.width2 * 1.5,
+                          height: context.width2 * 1.5,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: mainTheme.primaryColorLight,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Column(
+              children: [
+                SizedBox(height: context.height2 * 6),
+                Row(
+                  children: [
+                    _leftTabs == 0
+                        ? Text("Single Origin", style: mainTheme.textTheme.headline4)
+                        : _leftTabs == 1
+                            ? Text("Single Serve", style: mainTheme.textTheme.headline4)
+                            : _leftTabs == 2
+                                ? Text("Blend", style: mainTheme.textTheme.headline4)
+                                : _leftTabs == 3
+                                    ? Text("Options Page", style: mainTheme.textTheme.headline4)
+                                    : null,
+                    Spacer(),
+                    IconButton(
+                      splashRadius: 20,
+                      onPressed: () {},
+                      iconSize: context.iconSmall,
+                      icon: Icon(Icons.shopping_basket),
+                    ),
+                  ],
+                ),
+                SizedBox(height: context.height2 * 6),
+                Expanded(
+                  child: PageView(
+                    scrollDirection: Axis.vertical,
+                    controller: _myPageController,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: _leftTabs == 3
+
+                        ///i "just" can see "TEXT Page" text in Enable Widget Select Mode? :/
+                        ? [Text("Options Page", style: mainTheme.textTheme.headline4)]
+                        : coffeesType
+                            .map((everyItems) => PageView(
+                                  scrollDirection: Axis.horizontal,
+                                  children: everyItems.map<Widget>((e) => ProductWidget(e)).toList(),
+                                ))
+                            .toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -191,9 +210,10 @@ class _SecondLeftBarState extends State<SecondLeftBar> {
   }
 
   void _tabChecked(int c) {
-    setState(() {
-      _checkTab = c;
-    });
+    _myTabController.jumpToPage(c);
+    // setState(() {
+    // _checkTab = c;
+    //});
   }
 }
 
@@ -233,12 +253,10 @@ class _menuTextWidget extends StatelessWidget {
 
 class BottomBarWidget extends StatelessWidget {
   final void Function(int current) currentTab;
-  final int c;
 
   const BottomBarWidget({
     Key key,
     @required this.currentTab,
-    this.c,
   }) : super(key: key);
 
   @override
@@ -260,7 +278,7 @@ class BottomBarWidget extends StatelessWidget {
               height: context.height2 * 5,
               width: context.width2 * 30,
               onTap: () {
-                currentTab(c);
+                currentTab(index);
               },
             ),
             SizedBox(
